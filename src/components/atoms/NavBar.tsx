f89e40531b17c090a8047985699ef20c3cb19caf
RikuGhi <rifkhairkhairur@gmail.com>
2023-04-27 09:38:39 +0700

    add simple ui for posts

---
+++ src/components/atoms/NavBar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const currentRoute = usePathname();
+  const groupMenus = [
+    [
+      {
+        name: "Home",
+        path: "/",
+      },
+      {
+        name: "Posts",
+        path: "/posts",
+      },
+    ],
+  ];
+
+  return (
+    <nav className="flex justify-between py-4">
+      {groupMenus.map((groupMenu, i) => (
+        <div className="space-x-2" key={i}>
+          {groupMenu.map((menu, i) => (
+            <Link
+              key={i}
+              href={menu.path}
+              className={menu.path == currentRoute ? "font-bold" : ""}
+            >
+              {menu.name}
+            </Link>
+          ))}
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
