e237c5f56539bb27a37c8dab341e8c97f7bf7cba
RikuGhi <rifkhairkhairur@gmail.com>
2023-04-27 13:31:27 +0700

    add create post

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,4 @@
+import { z } from "zod"
 import {
   createTRPCRouter,
   publicProcedure,
@@ -12,4 +13,20 @@ export const postRouter = createTRPCRouter({
       },
     })
   }),
+  create: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.post.create({
+        data: {
+          title: input.title,
+          content: input.content,
+          userId: "b1",
+        },
+      })
+    }),
 })
